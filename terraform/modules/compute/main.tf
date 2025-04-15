3fe0340919a2ca8de5e87429bedad64fd61d7b6f
Bibek Pandey <36302659+bibekhazard@users.noreply.github.com>
2025-04-15 06:19:04 -0400

    Update main.tf

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,10 +1,9 @@
-data "aws_ami" "latest_amazon_linux_2023" {
+data "aws_ami" "latest_amazon_linux" {
   owners      = ["amazon"]
   most_recent = true
-
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 
