33b0f760f0d276473bff504578185f9c0df99081
Bibek Pandey <36302659+bibekhazard@users.noreply.github.com>
2025-04-15 05:59:37 -0400

    Update webserver.sh

--- terraform/modules/compute/user_data/webserver.sh
+++ terraform/modules/compute/user_data/webserver.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 yum update -y
+amazon-linux-extras enable python3.8
+yum clean metadata
 yum install -y python3.8 httpd
+
 # Set Python 3.8 as default
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 alternatives --set python3 /usr/bin/python3.8
-# Verify Python version
-python3 --versio
+
 systemctl start httpd
 systemctl enable httpd
 
