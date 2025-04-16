72eb45d4c2e73f898f00c127644f2f7b5d944deb
SUNGOLDSV <32132530+SUNGOLDSV@users.noreply.github.com>
2025-04-16 02:21:42 -0400

    Update S3 backend

--- terraform/environments/production/backend.tf
+++ terraform/environments/production/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "acs-final-project"
+    bucket         = "acs730-final-proj2"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
